26105cb4d54ee2586e59e7c50627ded991c6d82a
Damiano Derin <41470459+damiano1996@users.noreply.github.com>
2023-09-30 23:22:50 +0200

    jupyter notebook and renderer updated (#2)

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ Setup of the project.
 
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
 setup(
     name='gym_trading',
-    version='0.0.0',
-    install_requires=['gym']
+    version='0.0.1',
+    install_requires=required_packages
 )
